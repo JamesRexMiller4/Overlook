4d2bc275ffe962e180af0f4872daa19183f2ef6c
James Miller <james.rex.miller.4@gmail.com>
2019-11-01 22:13:22 -0600

    Create hotel class, no methods

--- src/Hotel.js
+++ src/Hotel.js
@@ -1,6 +1,7 @@
 class Hotel {
-  constructor(dateObj, rooms, bookings) {
+  constructor(dateObj, users, rooms, bookings) {
     this.date = dateObj;
+    this.users = users;
     this.rooms = rooms;
     this.bookings = bookings;
   }
@@ -10,6 +11,10 @@ class Hotel {
     let month = this.date.getMonth() + 1
     let day = String(this.date.getDate()).padStart(2, '0')
     this.date = `${year}/${month}/${day}`
+  }
+
+  findCurrentUser(id) {
+    return this.users.find(user => user.id === id)
   }
 
 }
