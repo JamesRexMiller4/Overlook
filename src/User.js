f64973f46c881e65f26b24e27e9c23d018ca313f
James Miller <james.rex.miller.4@gmail.com>
2019-11-01 18:31:06 -0600

    Create class files

---
+++ src/User.js
@@ -0,0 +1,19 @@
+class User {
+  constructor(id, username) {
+    this.id = id
+    this.username = username
+}
+
+  findAllRoomsAvailableByDate(date = currentDate) {
+      
+  }
+
+  findCustomerBookingHistory() {
+
+  }
+
+  findCustomerSpendingHistory() {
+
+  }
+  
+}
