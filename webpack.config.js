23d2bf9baac4a69073061b7d9a25e1ea7bc0b1a9
James Miller <james.rex.miller.4@gmail.com>
2020-01-05 10:14:44 -0700

    Add publicPath to webpack config file

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,7 @@ module.exports = {
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
+    publicPath: '/',
     filename: 'main.bundle.js'
   },
   devtool: 'inline-source-map',
